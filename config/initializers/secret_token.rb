be79b61df0937ab91e813614155ca96fe3481cfd
Rodrigo Lazoti <rodrigolazoti@gmail.com>
2010-09-11 13:47:30 -0300

    Complete example

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Contacts::Application.config.secret_token = '0f86714394e295a586de9bd5d1066ca94560dcc75b31e2ca23cf8a868d1d5d6811ffb1f6ef3ccd5b407a5c731dbfbc777c460761894ec9f3c89c60e7f83ae851'
---
+++ db/migrate/20100911161947_create_contacts.rb
@@ -0,0 +1,16 @@
+class CreateContacts < ActiveRecord::Migration
+  def self.up
+    create_table :contacts do |t|
+      t.string :name
+      t.string :email
+      t.date :birthdate
+      t.text :bio
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :contacts
+  end
+end
